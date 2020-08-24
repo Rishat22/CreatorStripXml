12d6411b3bb586132c6d7ad849bcf38531d1316f
Rishat Zakirov <zakirovrishat.me@gmail.com>
2020-08-24 13:01:22 +0200

    delete default load

--- CMainWidget.cpp
+++ CMainWidget.cpp
@@ -15,13 +15,12 @@ CMainWidget::CMainWidget(QWidget *parent)
 	mainLayout->addWidget(m_wdgSelectionMenu);
 
 	connect(m_wdgSelectionMenu, &CWdgSelectionMenu::addItemToScene, m_wdgScene, &CWdgScene::addItemToScene);
-//	connect(m_wdgSelectionMenu, &CWdgSelectionMenu::overwriteItemFromScene, m_wdgScene, &CWdgScene::overwriteItemFromScene);
+	connect(m_wdgSelectionMenu, &CWdgSelectionMenu::overwriteItemFromScene, m_wdgScene, &CWdgScene::overwriteItemFromScene);
 	connect(m_wdgSelectionMenu, &CWdgSelectionMenu::deleteItemFromScene, m_wdgScene, &CWdgScene::deleteItemFromScene);
 	connect(m_wdgScene, &CWdgScene::selectedItemConfig, m_wdgSelectionMenu, &CWdgSelectionMenu::setStripItemConfig);
 	connect(m_wdgSelectionMenu, &CWdgSelectionMenu::loadData, this, &CMainWidget::loadData);
 	connect(m_wdgSelectionMenu, &CWdgSelectionMenu::saveData, this, &CMainWidget::saveData);
 	setLayout(mainLayout);
-	loadData("/home/userme/Документы/NewFile.xml");
 }
 
 CMainWidget::~CMainWidget()
